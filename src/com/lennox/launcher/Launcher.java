a0f32e4e4b19a4b83a307d24f65a58fa0d455ebb
lenny_kano <lennykano@gmail.com>
2014-05-04 09:44:10 +0930

    Add pollfish support

--- src/com/lennox/launcher/Launcher.java
+++ src/com/lennox/launcher/Launcher.java
@@ -122,7 +122,7 @@ import java.util.Set;
 /**
  * Default launcher application.
  */
-public final class Launcher extends Activity
+public final class Launcher extends com.lennox.pollfish.PollFishActivity
         implements View.OnClickListener, OnLongClickListener, LauncherModel.Callbacks,
                    View.OnTouchListener {
     static final String TAG = "Launcher";
@@ -353,7 +353,7 @@ public final class Launcher extends Activity
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         if (DEBUG_STRICT_MODE) {
             StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                     .detectDiskReads()
